40db1a071cefadbed37aa8b875dfdc5dee5eace6
Sonia806 <79003788+Sonia806@users.noreply.github.com>
2021-03-31 19:15:26 +0530

    Major Updates

--- configs.py
+++ configs.py
@@ -13,27 +13,27 @@ class Config(object):
 	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", None)
 	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
-Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
+This is 𝗘𝘃𝗲𝗹𝘆𝗻 𝗙𝗶𝗹𝗲 𝗦𝗵𝗮𝗿𝗲 & 𝗦𝘁𝗼𝗿𝗮𝗴𝗲  𝗥𝗼𝗯𝗼𝘁!
+Send me any file I will save it in my Database Permanently. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
 🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
 
 📝 **Language:** [Python3](https://www.python.org)
 
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
-
 📡 **Hosted on:** [Heroku](https://heroku.com)
 
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @Sonia_Mohapatra
 
-👥 **Support Group:** [Linux Repositories](https://t.me/linux_repo)
+👥 **Support Group:** [🏅 Movie Buzz ®](https://t.me/MovieBuzzOfficial)
 
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+📢 **Updates Channel:** [Movie Buzz Group🏩](https://t.me/Discovery_Updates)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+🧑🏻‍💻 **Developer:** @Sonia_Mohapatra
 
-Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
+Hello. I Will Not Brag About Something Which I Didnt Do. This Bot Was Officially Made By Abir Hasan.
+And A Big Thank You To Him For Making This Bot. You Can Donate To Him By Clicking On The Link Given Below.
+And Again A Big Thank You To The Bot Makers. Cheers! ! Please Donate the developer for Keeping the Service Alive.
 
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
 
@@ -42,5 +42,15 @@ Also remember that developer will Delete Adult Contents from Database. So better
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
 
-Send me any file I will give you a permanent Sharable Link. I Support Channel Also! Check **About Bot** Button.
+𝗛𝗲𝘆 𝗗𝗲𝗮𝗿 𝗜 𝗮𝗺 𝗘𝘃𝗲𝗹𝘆𝗻 𝗙𝗶𝗹𝗲 𝗦𝗵𝗮𝗿𝗲 & 𝗦𝘁𝗼𝗿𝗮𝗴𝗲  𝗥𝗼𝗯𝗼𝘁 😁
+
+📌𝗛𝗢𝗪 𝗧𝗢 𝗨𝗦𝗘 & 𝗪𝗛𝗔𝗧 𝗕𝗘𝗡𝗜𝗙𝗜𝗧??
+
+📫Send Me any File i can upload
+Your File in my Top secret Database 🤗 I will give you a permanent Sharable Link. I Support Channel Also!
+
+⭕Benifit:  If You Have Telegram Movie Channel! Then Useful for you.You can Send me your File and I'll send you link of your file, So people will get File From me 😊 and Your Channel will safe NO COPYRIGHT issue🤩.
+😎 Availability : Only people with the link can access the file. No one in the universe can access your file without your permission.
+🔵𝗗𝗼𝗻'𝘁 𝗨𝘀𝗲 𝗣𝗼𝗿𝗻𝗼𝗴𝗿𝗮𝗽𝗵𝘆 𝗙𝗶𝗹𝗲
+𝗢𝘁𝗵𝗲𝗿𝘄𝗶𝘀𝗲 𝘆𝗼𝘂 𝘄𝗶𝗹𝗹 𝗯𝗲 𝗯𝗮𝗻 Check **About Bot** Button.
 """
